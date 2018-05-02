3588bdc345ffd8de5fa5ac21f54121c9d751bb7a
Niels Vandekeybus <progster@gmail.com>
2018-05-02 10:48:28 +0200

    better error handling

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-import { app, query, update, uuid, sparqlEscapeString, sparqlEscapeDate, sparqlEscapeUri } from 'mu';
+import { app } from 'mu';
 import { fetchSession, createReport, hasValidBody } from './support';
 app.post('/bbcdr-rapporten/', async function( req, res ) {
   try {
-  const sessionURI = req.headers['mu-session-id'];
-  const activeSession = await fetchSession(sessionURI);
-  const body = req.body;
-    if (hasValidBody(body)) {
-      const fileIdentifiers = body.data.relationships.files.data.map((obj) => obj.id);
-      const report = createReport(activeSession, fileIdentifiers);
-      res.status(201).send(report);
+    const sessionURI = req.headers['mu-session-id'];
+    const activeSession = await fetchSession(sessionURI);
+    if (activeSession) {
+      const body = req.body;
+      if (hasValidBody(body)) {
+        const fileIdentifiers = body.data.relationships.files.data.map((obj) => obj.id);
+        const report = createReport(activeSession, fileIdentifiers);
+        res.status(201).send(report);
+      }
+      else {
+        res.status(400).send({status: 400, title:'request is invalid'});
+      }
     }
     else {
-      res.status(400).send({status: 400, title:'request is invalid'});
+      res.status(401).send({status: 401, title: 'could not find an account linked to this session'});
     }
   }
   catch(e) {
