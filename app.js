314ee4f570a2aadeae7a80cc3dc3695e2f093aad
Niels Vandekeybus <progster@gmail.com>
2018-05-08 15:01:55 +0200

    support patch

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import { app } from 'mu';
-import { fetchSession, createReport, hasValidBody } from './support';
+import { fetchSession, createReport, hasValidCreateBody, hasValidPatchBody, fetchReport, updateReport } from './support';
 app.post('/bbcdr-reports/', async function( req, res ) {
   try {
     const sessionURI = req.headers['mu-session-id'];
     const activeSession = await fetchSession(sessionURI);
     if (activeSession) {
       const body = req.body;
-      if (hasValidBody(body)) {
+      if (hasValidCreateBody(body)) {
         const fileIdentifiers = body.data.relationships.files.data.map((obj) => obj.id);
         const report = await createReport(activeSession, fileIdentifiers);
         res.status(201).send(report);
@@ -17,6 +17,37 @@ app.post('/bbcdr-reports/', async function( req, res ) {
     }
     else {
       res.status(401).send({status: 401, title: 'could not find an account linked to this session'});
+    }
+  }
+  catch(e) {
+    console.error(e);
+    res.status(500).send({status: 500, title: 'unexpected error while processing request'});
+  }
+});
+
+app.patch('/bbcdr-reports/:id', async function( req, res ) {
+  try {
+    const sessionURI = req.headers['mu-session-id'];
+    const activeSession = await fetchSession(sessionURI);
+    // bail out if we have no account
+    if (!activeSession) {
+      res.status(401).send({status: 401, title: 'could not find an account linked to this session'}).end();
+    }
+
+    const report = await fetchReport(req.params.id);
+    // bail out if we can't find the report
+    if (report.length === 0) {
+      console.log(report);
+      res.status(404).send({status: 404, title: 'not found'}).end();
+    }
+
+    const body = req.body;
+    if (hasValidPatchBody(body)) {
+      const updatedReport = await updateReport(report[0], req.params.id, body.data.relationships, activeSession);
+      res.status(200).send(updatedReport);
+    }
+    else {
+      res.status(400).send({status: 400, title: 'request is invalid'});
     }
   }
   catch(e) {
--- support.js
+++ support.js
@@ -1,6 +1,21 @@
 import { app, query, update, uuid, sparqlEscapeString, sparqlEscapeDateTime, sparqlEscapeUri } from 'mu';
 const BASE_IRI = process.env.MU_BASE_IRI || "http://example.org/reports/";
 
+
+/**
+ * convert results of select query to an array of objects.
+ * @method parseResult
+ * @return {Array}
+ */
+const parseResult = function(result) {
+  const bindingKeys = result.head.vars;
+  return result.results.bindings.map((row) => {
+    const obj = {};
+    bindingKeys.forEach((key) => obj[key] = row[key].value);
+    return obj;
+  });
+};
+
 /**
  * fetch the current user and group linked to the SessionIRI
  *
@@ -24,9 +39,183 @@ const  fetchSession = async function(sessionURI) {
     return null;
   }
   else {
-    const r = result.results.bindings[0];
-    return { user: r.user.value, group: r.group.value, groupID: r.groupID.value, userID: r.userID.value};
+    return parseResult(result)[0];
   }
+};
+
+/**
+ * fetch a report and its properties based on its uuid
+ * @method fetchReport
+ */
+const fetchReport = async function(reportId) {
+  const result = await query(`
+    PREFIX mu:   <http://mu.semte.ch/vocabularies/core/>
+    PREFIX ext:   <http://mu.semte.ch/vocabularies/ext/>
+    PREFIX nie:     <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
+    PREFIX nfo: <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#>
+    PREFIX dcterms: <http://purl.org/dc/terms/>
+    PREFIX adms:    <http://www.w3.org/ns/adms#>
+    PREFIX bbcdr: <http://mu.semte.ch/vocabularies/ext/bbcdr/>
+    SELECT ?reportIRI ?status ?statusID ?created ?modified ?lastModifiedBy ?subject
+    FROM <http://mu.semte.ch/application>
+    WHERE {
+      ?reportIRI a bbcdr:Report;
+                 adms:status ?status;
+                 dcterms:created ?created;
+                 dcterms:modified ?modified;
+                 ext:lastModifiedBy ?lastModifiedBy;
+                 dcterms:subject ?subject;
+                 mu:uuid ${sparqlEscapeString(reportId)}.
+     ?status mu:uuid ?statusID.
+   }
+  `);
+  return parseResult(result);
+};
+
+const fetchFilesForReport = async function(reportIRI) {
+  const files = await query(`
+       PREFIX nie:     <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
+       PREFIX mu:   <http://mu.semte.ch/vocabularies/core/>
+       PREFIX nfo: <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#>
+       PREFIX bbcdr: <http://mu.semte.ch/vocabularies/ext/bbcdr/>
+       SELECT ?iri ?uuid
+       FROM <http://mu.semte.ch/application>
+       WHERE {
+         ${reportIRI} a bbcdr:Report;
+               nie:hasPart ?iri.
+         ?iri mu:uuid ?uuid.
+       }`);
+  return parseResult(files);
+};
+const fetchFiles = async function(fileIdentifiers) {
+  const files = await query(`
+       PREFIX mu:   <http://mu.semte.ch/vocabularies/core/>
+       PREFIX nfo: <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#>
+       SELECT ?file ?uuid
+       FROM <http://mu.semte.ch/application>
+       WHERE {
+         ?file a nfo:FileDataObject;
+               mu:uuid ?uuid.
+         FILTER(?uuid IN ( ${fileIdentifiers.map((id) => sparqlEscapeString(id)).join(',')}))
+       }`);
+  return parseResult(files);
+};
+
+const fetchStatus = async function(id) {
+  const result = await query(`
+       PREFIX mu:   <http://mu.semte.ch/vocabularies/core/>
+       PREFIX nfo: <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#>
+       SELECT ?concept
+       FROM <http://mu.semte.ch/application>
+       WHERE {
+         ?concept mu:uuid ${sparqlEscapeString(id)}.
+      }
+`);
+  return parseResult(result)[0].concept;
+};
+const updateReport = async function(report, reportID, relationships, activeSession) {
+  const deleteStatements = [];
+  const insertStatements = [];
+  const reportIRI = sparqlEscapeUri(report.reportIRI);
+  const files = await fetchFilesForReport(reportIRI);
+  let fileIdentifiers = files.map((file) => file.uuid);
+  let statusID = report.statusID;
+  if (relationships.files) {
+    files.forEach((file) => deleteStatements.push(`${reportIRI} nie:hasPart ${sparqlEscapeUri(file.iri)}`));
+    const newFiles = await fetchFiles(relationships.files.data.map((file) => file.id));
+    fileIdentifiers = newFiles.map((file) => file.uuid);
+    newFiles.forEach((file) => {
+      insertStatements.push(`${reportIRI} nie:hasPart ${sparqlEscapeUri(file.file)}`);
+    });
+  }
+  if (relationships.status) {
+    deleteStatements.push(`${reportIRI} adms:status ${sparqlEscapeUri(report.status)}`);
+    const status = await fetchStatus(relationships.status.data.id);
+    statusID = relationships.status.data.id;
+    insertStatements.push(`${reportIRI} adms:status ${sparqlEscapeUri(status)}`);
+  }
+  const now = new Date();
+  await update(`
+       PREFIX session: <http://mu.semte.ch/vocabularies/session>
+       PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+       PREFIX mu:   <http://mu.semte.ch/vocabularies/core/>
+       PREFIX ext:   <http://mu.semte.ch/vocabularies/ext/>
+       PREFIX nie:     <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
+       PREFIX nfo: <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#>
+       PREFIX dcterms: <http://purl.org/dc/terms/>
+       PREFIX adms:    <http://www.w3.org/ns/adms#>
+       PREFIX bbcdr: <http://mu.semte.ch/vocabularies/ext/bbcdr/>
+       WITH <http://mu.semte.ch/application>
+       DELETE {
+          ${reportIRI} dcterms:modified ?modified;
+                       dcterms:subject ?subject;
+                       ext:lastModifiedBy ?lastModified.
+          ${deleteStatements.join(".\n")}
+       }
+       INSERT {
+          ${reportIRI} dcterms:modified ${sparqlEscapeDateTime(now)};
+                       dcterms:subject ${sparqlEscapeUri(activeSession.group)};
+                       ext:lastModifiedBy ${sparqlEscapeUri(activeSession.user)}.
+          ${insertStatements.join(".\n")}
+       }
+       WHERE {
+          ${reportIRI} dcterms:modified ?modified;
+                       dcterms:subject ?subject;
+                       ext:lastModifiedBy ?lastModified.
+       }
+`);
+  return buildJSONAPIResponse(
+    reportID,
+    new Date(report.created),
+    now,
+    fileIdentifiers,
+    statusID,
+    activeSession.userID,
+    activeSession.groupID
+  );
+};
+
+const buildJSONAPIResponse = function(reportID, created, modified, fileIdentifiers, statusID, userID, groupID) {
+  const filesJSON = fileIdentifiers.map( (id) => {
+    return {
+      links: {self: `/files/${id}`},
+      data: {type: 'files', id: id}
+    };
+  });
+  return {
+    links: {
+      self: `/bbcdr-reports/${reportID}`
+    },
+    data: {
+      attributes: {
+        created: created,
+        modified: modified
+      },
+      relationships: {
+        files: filesJSON,
+        documentStatus: {
+          "links": {
+            "self": `/document-statuses/${statusID}`
+          },
+          "data": { "type": "document-statuses", "id": statusID }
+        },
+        gebruiker: {
+          "links": {
+            "self": `/gebruikers/${userID}`
+          },
+          "data": { "type": "gebruikers", "id": userID }
+        },
+        bestuurseenheid: {
+          "links": {
+            "self": `/bestuurseenheid/${groupID}`
+          },
+          "data": { "type": "bestuurseenheden", "id": groupID }
+        }
+      },
+      id: reportID,
+      type: 'bbcdr-reports'
+    }
+  };
 };
 
 /**
@@ -38,7 +227,7 @@ const  fetchSession = async function(sessionURI) {
 const createReport = async function(activeSession, fileIdentifiers) {
   const id = uuid();
   const now = new Date();
-  const reportIRI = `${BASE_IRI}/${id}`;
+  const reportIRI = `${BASE_IRI}${id}`;
   const draft = 'http://data.lblod.info/document-statuses/concept';
   const draftID = 'concept';
   const withFiles = fileIdentifiers.length > 0;
@@ -73,46 +262,31 @@ const createReport = async function(activeSession, fileIdentifiers) {
        :
        ''
        }`);
-  const filesJSON = fileIdentifiers.map( (id) => {
-    return {
-      links: {self: `/files/${id}`},
-      data: {type: 'files', id: id}
-    };
-  });
-  return {
-    links: {
-      self: `/bbcdr-reports/${id}`
-    },
-    data: {
-      attributes: {
-        created: now,
-        modified: now
-      },
-      relationships: {
-        files: filesJSON,
-        documentStatus: {
-          "links": {
-            "self": `/document-statuses/${draftID}`
-          },
-          "data": { "type": "document-statuses", "id": draftID }
-        },
-        gebruiker: {
-          "links": {
-            "self": `/gebruikers/${activeSession.userID}`
-          },
-          "data": { "type": "gebruikers", "id": activeSession.userID }
-        },
-        bestuurseenheid: {
-          "links": {
-            "self": `/bestuurseenheid/${activeSession.groupID}`
-          },
-          "data": { "type": "bestuurseenheden", "id": activeSession.groupID }
-        }
-      },
-      id: id,
-      type: 'bbcdr-reports'
-    }
-  };
+  return buildJSONAPIResponse(id, now, now, fileIdentifiers, draftID, activeSession.userID, activeSession.groupID );
+};
+
+const hasValidBody = function(body) {
+  if (!body.data) return false;
+  const data = body.data;
+  if (data.type !== "bbcdr-reports") return false;
+  return true;
+}
+
+/**
+ * validate the request body
+ */
+const hasValidPatchBody = function(body) {
+  if(!hasValidBody(body)) return false;
+  if(!body.data.id) return false;
+  if (body.data.relationships && body.data.relationships.files) {
+    if(!body.data.relationships.files.data || !body.data.relationships.files.data instanceof Array)
+      return false;
+  }
+  if (body.data.relationships && body.data.relationships.status) {
+    if(!body.data.relationships.status.data)
+      return false;
+  }
+  return true;
 };
 
 /**
@@ -121,13 +295,11 @@ const createReport = async function(activeSession, fileIdentifiers) {
  * @method hasValidBody
  * @return {boolean}
  */
-const hasValidBody = function(body) {
-  if (!body.data) return false;
-  const data = body.data;
-  if (data.type !== "bbcdr-reports") return false;
+const hasValidCreateBody = function(body) {
+  if (!hasValidBody(body)) return false;
   if (!body.data.relationships) return false;
   if (!body.data.relationships.files) return false;
   return true;
 };
 
-export { hasValidBody, fetchSession, createReport };
+export { hasValidCreateBody, hasValidPatchBody, fetchSession, createReport, fetchReport, updateReport };
